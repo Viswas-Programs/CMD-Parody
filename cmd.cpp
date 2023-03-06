0117fb27922b86f3b0c7ca1ca17d47b3c9f74bd7
Viswas <viswas.anand@outlook.com>
2023-03-06 23:19:38 +0530

    Added file output support
    In echo command, you now can use filenames (like example.txt) as an output stream to the "specify output stream" prompt, and, everything you've written will be outputted to the file.
    This uses std::ofstream and includes fstream lib

--- cmd.cpp
+++ cmd.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
@@ -7,19 +8,21 @@ std::string print(std::string str, bool toIncludeEndl){
     if (toIncludeEndl){std::cout << str << std::endl;}
     else {std::cout << str;}
     return str;}
-std::string winver(){print("Windows 11 22H2 (Build 22621.1265)\nCommand Prompt v0.1", true); return "Hi";}
-std::string changeDir(std::string path){currentWorkingDirectory += path; return "Hi too";}
+std::string winver(){print("Windows 11 22H2 (Build 22621.1265)\nCommand Prompt v0.1", true); return "winver()";}
+std::string changeDir(std::string path){currentWorkingDirectory += path; return "changeDir()"; }
 std::string about(){return "This is a C++ version of my existing Python CMD, this, here is a parody command prompt!\nChanges made here, won't affect your system\n";}
-std::string echo() { print(">_", false); std::string line1{};  getline(std::cin, line1); print("specify output stream>", false); std::string output{}; std::cin >> output; if (output == "stdout" || output == "cout" || output == "std::cout") { print(line1, true); } return "echo function!"; }
+std::string echo() { print(">_", false); std::string line1{};  getline(std::cin, line1); print("specify output stream>", false); std::string output{}; std::cin >> output; if (output == "stdout" || output == "cout" || output == "std::cout") { print(line1, true); }
+else { std::ofstream os(output); os << line1; os.close(); }
+                    return "echo function!";}
 int main(){
-    std::string RealUsername {"FooUser1"};
-    std::string RealPassword {"1234!@#$"};
+    const char RealUsername[9] {"FooUser1"};
+    const char RealPassword[9] {"1234!@#$"};
     std::string inputtedUsername {};
     std::string inputtedPassword {};
     print("Please enter your username: ", false);
     getline(std::cin, inputtedUsername);
     // std::cin.getline(0, 50) >> inputtedUsername;
-    print("\nPlease enter your password: ", false);
+    print("Please enter your password: ", false);
     getline(std::cin, inputtedPassword);
     // std::cin.getline(0, 50) >> inputtedPassword;
     if (inputtedUsername == RealUsername && inputtedPassword == RealPassword){
@@ -36,7 +39,7 @@ int main(){
             else if (enteredCommand == "echo") { echo(); }
             else {print("Command '" + enteredCommand + "' not recognized", true); }
     }}
-    else{print("The username or password or both was incorrect, rerun the program and enter the correct credentials! Press any key to restart!>", true); std::string i {};std::cin>> i;}
+    else { print("The username or password or both was incorrect, rerun the program and enter the correct credentials! Press any key to restart!>", true); std::string i{}; std::cin >> i; return 0; }
 
     return 0;
 }
